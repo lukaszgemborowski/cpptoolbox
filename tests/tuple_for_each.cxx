05e13b572de534cd35f088719f0f8406438b0fe1
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-08-27 16:15:38 +0200

    tests: tuple_for_each: how to conditionaly change element
    based on tuple element type

--- tests/tuple_for_each.cxx
+++ tests/tuple_for_each.cxx
@@ -39,3 +39,35 @@ TEST_CASE("Functor with state", "[tuple_for_each][tuple]")
 
 	REQUIRE(fun.sum == 6);
 }
+
+struct int_doubler
+{
+	template<typename T, typename U, typename R = void>
+	using enable_for_type_t = typename std::enable_if<std::is_same<T, U>::value>::type;
+	template<typename T, typename U, typename R = void>
+	using disable_for_type_t = typename std::enable_if<!std::is_same<T, U>::value>::type;
+
+	template<typename T>
+	enable_for_type_t<T, int> operator()(T &arg)
+	{
+		arg *= 2;
+	}
+
+	template<typename T>
+	disable_for_type_t<T, int> operator()(T &) {}
+};
+
+TEST_CASE("Selective modification", "[tuple_for_each][tuple]")
+{
+	auto tuple = std::make_tuple(1, 2, 3.0, 4.0, 5.f, 6.f);
+	const auto orig = tuple;
+
+	cpp::tuple_for_each(tuple, int_doubler{});
+
+	REQUIRE(std::get<0>(tuple) == 2);
+	REQUIRE(std::get<1>(tuple) == 4);
+	REQUIRE(std::get<2>(tuple) == 3.0);
+	REQUIRE(std::get<3>(tuple) == 4.0);
+	REQUIRE(std::get<4>(tuple) == 5.f);
+	REQUIRE(std::get<5>(tuple) == 6.f);
+}
