0632eb71fa2fc7bb9c28f16f7b6afddc50dbcb2d
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-09-10 17:21:06 +0200

    cpp: for_each_param function template defined

---
+++ include/toolbox/cpp/for_each_param.hpp
@@ -0,0 +1,37 @@
+#ifndef _TOOLBOX_CPP_FOR_EACH_PARAM_HPP_
+#define _TOOLBOX_CPP_FOR_EACH_PARAM_HPP_
+
+namespace toolbox
+{
+namespace cpp
+{
+
+namespace detail
+{
+
+template<typename Func, typename Last>
+void for_each_param(Func &&func, Last &&last)
+{
+	func(last);
+}
+
+template<typename Func, typename First, typename... Params>
+void for_each_param(Func &&func, First &&first, Params&&... params)
+{
+	func(first);
+	for_each_param(func, params...);
+}
+
+} // namespace detail
+
+template<typename Func, typename... Params>
+auto for_each_param(Func &&func, Params&&... params)
+{
+	detail::for_each_param(func, params...);
+	return func;
+}
+
+} // namespace cpp
+} // namespace toolbox
+
+#endif
