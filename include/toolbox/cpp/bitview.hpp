e2557d9728c733b7751c5672a4a5b60cb61793b2
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-11-08 17:24:04 +0100

    bitview: implementation and tests

---
+++ include/toolbox/cpp/bitview.hpp
@@ -0,0 +1,78 @@
+#ifndef _CPP_BITVIEW_HPP_
+#define _CPP_BITVIEW_HPP_
+
+namespace toolbox
+{
+namespace cpp
+{
+
+template<typename IntT, std::size_t I>
+using onebit = std::integral_constant<IntT, 1 << I>;
+
+template<typename IntT, std::size_t N>
+struct mask : public std::integral_constant<
+	IntT, mask<IntT, N - 1>::value | onebit<IntT, N - 1>::value> {};
+
+template<typename IntT>
+struct mask<IntT, 0> : public std::integral_constant<IntT, 1> {};
+
+
+namespace detail
+{
+
+template<typename IntT, std::size_t Offset, std::size_t Size>
+class bitview_slice
+{
+public:
+	bitview_slice(IntT &value) :
+		value_ (value)
+	{}
+
+	void operator=(const IntT &v)
+	{
+		set(v);
+	}
+
+	void set(const IntT &v)
+	{
+		// clear field
+		value_ = value_ & ~(mask<IntT, Size>::value << Offset);
+
+		// set new value
+		value_ |= (v & mask<IntT, Size>::value) << Offset;
+	}
+
+private:
+	IntT &value_;
+};
+
+} // namespace detail
+
+template<typename IntRefT>
+class bitview
+{
+public:
+	bitview(IntRefT value) :
+		value_ (value)
+	{}
+
+	template<std::size_t Offset, std::size_t Size>
+	auto slice()
+	{
+		return detail::bitview_slice<
+			typename std::remove_reference<IntRefT>::type, Offset, Size>(value_);
+	}
+
+	typename std::remove_reference<IntRefT>::type value() const
+	{
+		return value_;
+	}
+
+private:
+	IntRefT value_;
+};
+
+} // namespace cpp
+} // namespace toolbox
+
+#endif
---
+++ tests/bitview.cxx
@@ -0,0 +1,36 @@
+#include "catch.hpp"
+#include <toolbox/cpp/bitview.hpp>
+
+TEST_CASE("set one bit", "[onebit]")
+{
+	using first_bit = toolbox::cpp::onebit<int, 0>;
+	using second_bit = toolbox::cpp::onebit<int, 1>;
+	using third_bit = toolbox::cpp::onebit<int, 2>;
+
+	REQUIRE(first_bit::value == 1);
+	REQUIRE(second_bit::value == 2);
+	REQUIRE(third_bit::value == 4);
+}
+
+TEST_CASE("bit mask", "[mask]")
+{
+	using mask_three = toolbox::cpp::mask<int, 3>;
+	using mask_four = toolbox::cpp::mask<int, 4>;
+
+	REQUIRE(mask_three::value == 7);
+	REQUIRE(mask_four::value == 0xf);
+}
+
+TEST_CASE("Slicing an int", "[bitview]")
+{
+	int value = 0;
+	toolbox::cpp::bitview<int &> bv(value);
+	auto slice = bv.slice<4, 4>();
+	slice = 5;
+
+	REQUIRE(value == 0x50);
+
+	bv.slice<16, 8>() = 0xab;
+
+	REQUIRE(value == 0xab0050);
+}
