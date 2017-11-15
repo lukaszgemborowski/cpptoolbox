5cd7f936db8932990be48e99ee51bf616d912cc3
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2017-11-15 13:36:51 +0100

    int_type meta class introduction

---
+++ tests/int_type.cxx
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+#include <toolbox/cpp/int_type.hpp>
+
+// static checks
+
+template<std::size_t N, typename T>
+using check = std::is_same<typename toolbox::cpp::int_type<N>::signed_type, T>;
+
+static_assert(check<1, std::int8_t>::value, "1 bit = char");
+static_assert(check<8, std::int8_t>::value, "8 bit = char");
+
+static_assert(check<9, std::int16_t>::value, "9 bit = int16");
+static_assert(check<16, std::int16_t>::value, "16 bit = int16");
+
+static_assert(check<17, std::int32_t>::value, "17 bit = int32");
+static_assert(check<32, std::int32_t>::value, "32 bit = int32");
+
+static_assert(check<33, std::int64_t>::value, "33 bit = int32");
+static_assert(check<64, std::int64_t>::value, "64 bit = int32");
