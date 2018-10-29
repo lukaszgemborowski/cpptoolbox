78db31ed9bf1cad7ad29eea5ebb6cbbebea84eb8
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2018-10-29 21:26:59 +0100

    generate config.h for the library and separate cmakelist for tests

---
+++ include/toolbox/config.h
@@ -0,0 +1,11 @@
+#ifndef _CPPTOOLBOX_CONFIG_H_
+#define _CPPTOOLBOX_CONFIG_H_
+
+// may be 98 (?), 11, 14, 17...
+#define TOOLBOX_CXX_STANDARD   14
+
+// do we have c++ standard library
+// for example gcc for AVR does not have C++ standard library
+#define TOOLBOX_HAS_STDLIB
+
+#endif
