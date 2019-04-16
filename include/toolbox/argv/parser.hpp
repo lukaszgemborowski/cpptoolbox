79d1495fe3382c55f14b5a8e1511d3450ea3611a
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-04-16 23:56:28 +0200

    extract non-generic part of parser ctor

--- include/toolbox/argv/parser.hpp
+++ include/toolbox/argv/parser.hpp
@@ -28,26 +28,32 @@ public:
         cpp::tuple_for_each(
             options_,
             [&ss, this](auto& option) {
-                ss << option.get_short();
-                if (option.has_argument()) ss << ":";
-
-                if (option.get_long().size() > 0) {
-                    ::option o = {
-                        option.get_long().c_str(),
-                        option.has_argument() ? required_argument : no_argument,
-                        nullptr,
-                        option.get_short()
-                    };
-
-                    options_arr_.push_back(o);
-                }
-
-                longest_name_ = std::max(longest_name_, option.get_long().size());
-                longest_argname_ = std::max(longest_argname_, option.get_argument_name().size());
+                updateOptions(ss, option, option.has_argument());
             }
         );
 
         opt_descriptor_ = ss.str();
+    }
+
+    void updateOptions(std::stringstream &ss, const options::base_option &option, bool has_argument)
+    {
+        ss << option.get_short();
+        if (has_argument)
+            ss << ":";
+
+        if (option.get_long().size() > 0) {
+            ::option o = {
+                option.get_long().c_str(),
+                has_argument ? required_argument : no_argument,
+                nullptr,
+                option.get_short()
+            };
+
+            options_arr_.push_back(o);
+        }
+
+        longest_name_ = std::max(longest_name_, option.get_long().size());
+        longest_argname_ = std::max(longest_argname_, option.get_argument_name().size());
     }
 
     void parse(int argc, char ** argv)
@@ -132,12 +138,12 @@ public:
     }
 
 private:
-    Options options_;
-    std::vector<::option> options_arr_;
-    std::string opt_descriptor_;
-    std::vector<std::string> non_options_;
-    std::size_t longest_name_ = 0;
-    std::size_t longest_argname_ = 0;
+    Options                     options_;
+    std::vector<::option>       options_arr_;
+    std::string                 opt_descriptor_;
+    std::vector<std::string>    non_options_;
+    std::size_t                 longest_name_ = 0;
+    std::size_t                 longest_argname_ = 0;
 };
 
 template<typename... Options>
