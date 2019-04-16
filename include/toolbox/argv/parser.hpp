283783dc39d69ba8b6355d7a13dab0bfb332e863
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-04-17 00:08:15 +0200

    argv: move part of argv parser to library

--- include/toolbox/argv/parser.hpp
+++ include/toolbox/argv/parser.hpp
@@ -17,8 +17,21 @@ namespace toolbox
 namespace argv
 {
 
+class base_parser
+{
+public:
+    void updateOptions(std::stringstream &ss, const options::base_option &option, bool has_argument);
+
+protected:
+    std::vector<::option>       options_arr_;
+    std::string                 opt_descriptor_;
+    std::vector<std::string>    non_options_;
+    std::size_t                 longest_name_ = 0;
+    std::size_t                 longest_argname_ = 0;
+};
+
 template<typename Options>
-class parser
+class parser : public base_parser
 {
 public:
     parser(Options &&options) :
@@ -35,26 +48,6 @@ public:
         opt_descriptor_ = ss.str();
     }
 
-    void updateOptions(std::stringstream &ss, const options::base_option &option, bool has_argument)
-    {
-        ss << option.get_short();
-        if (has_argument)
-            ss << ":";
-
-        if (option.get_long().size() > 0) {
-            ::option o = {
-                option.get_long().c_str(),
-                has_argument ? required_argument : no_argument,
-                nullptr,
-                option.get_short()
-            };
-
-            options_arr_.push_back(o);
-        }
-
-        longest_name_ = std::max(longest_name_, option.get_long().size());
-        longest_argname_ = std::max(longest_argname_, option.get_argument_name().size());
-    }
 
     void parse(int argc, char ** argv)
     {
@@ -138,12 +131,7 @@ public:
     }
 
 private:
-    Options                     options_;
-    std::vector<::option>       options_arr_;
-    std::string                 opt_descriptor_;
-    std::vector<std::string>    non_options_;
-    std::size_t                 longest_name_ = 0;
-    std::size_t                 longest_argname_ = 0;
+    Options options_;
 };
 
 template<typename... Options>
---
+++ src/argv/parser.cxx
@@ -0,0 +1,30 @@
+#include "toolbox/argv/parser.hpp"
+
+namespace toolbox
+{
+namespace argv
+{
+
+void base_parser::updateOptions(std::stringstream &ss, const options::base_option &option, bool has_argument)
+{
+    ss << option.get_short();
+    if (has_argument)
+        ss << ":";
+
+    if (option.get_long().size() > 0) {
+        ::option o = {
+            option.get_long().c_str(),
+            has_argument ? required_argument : no_argument,
+            nullptr,
+            option.get_short()
+        };
+
+        options_arr_.push_back(o);
+    }
+
+    longest_name_ = std::max(longest_name_, option.get_long().size());
+    longest_argname_ = std::max(longest_argname_, option.get_argument_name().size());
+}
+
+} // namespace argv
+} // namespace toolbox
