6e592909bebeb1692198c0b91c7978a0951969e3
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-04-22 14:14:01 +0200

    retab

--- include/toolbox/marshall/marshall.hpp
+++ include/toolbox/marshall/marshall.hpp
@@ -15,64 +15,64 @@ namespace detail
 template<typename S>
 struct record_serializer
 {
-	record_serializer(S &s) : s(s) {}
-	S& s;
+    record_serializer(S &s) : s(s) {}
+    S& s;
 
-	template<typename T> void enter(T &) {}
-	template<typename T> void leave(T &) {}
+    template<typename T> void enter(T &) {}
+    template<typename T> void leave(T &) {}
 
-	template<typename T>
-	typename std::enable_if<is_dynamic_array<typename T::type>::value>::type
-	visit(T &f)
-	{
-		typename T::type::array_size_t size = f.value.size();
-		s.write(reinterpret_cast<const char *>(&size), sizeof(size));
+    template<typename T>
+    typename std::enable_if<is_dynamic_array<typename T::type>::value>::type
+    visit(T &f)
+    {
+        typename T::type::array_size_t size = f.value.size();
+        s.write(reinterpret_cast<const char *>(&size), sizeof(size));
 
-		for (const auto &e : f.value) {
-			s.write(reinterpret_cast<const char*>(&e), sizeof(e));
-		}
-	}
+        for (const auto &e : f.value) {
+            s.write(reinterpret_cast<const char*>(&e), sizeof(e));
+        }
+    }
 
-	template<typename T>
-	typename std::enable_if<std::is_trivially_copyable<typename T::type>::value>::type
-	visit(T &f)
-	{
-		s.write(reinterpret_cast<const char *>(&f.value), sizeof(f.value));
-	}
+    template<typename T>
+    typename std::enable_if<std::is_trivially_copyable<typename T::type>::value>::type
+    visit(T &f)
+    {
+        s.write(reinterpret_cast<const char *>(&f.value), sizeof(f.value));
+    }
 };
 
 template<typename S>
 struct record_deserializer
 {
-	record_deserializer(S &s) : s(s) {}
-	S& s;
+    record_deserializer(S &s) : s(s) {}
+    S& s;
 
-	template<typename T> void enter(T &) {}
-	template<typename T> void leave(T &) {}
+    template<typename T> void enter(T &) {}
+    template<typename T> void leave(T &) {}
 
-	template<typename T>
-	typename std::enable_if<is_dynamic_array<typename T::type>::value>::type
-	visit(T &f)
-	{
-		using size_type = typename T::type::array_size_t;
+    template<typename T>
+    typename std::enable_if<is_dynamic_array<typename T::type>::value>::type
+    visit(T &f)
+    {
+        using size_type = typename T::type::array_size_t;
 
-		size_type size = f.value.size();
-		s.read(reinterpret_cast<char *>(&size), sizeof(size));
-		f.value.clear();
+        size_type size = f.value.size();
+        s.read(reinterpret_cast<char *>(&size), sizeof(size));
+        f.value.clear();
 
-		for (size_type i = 0; i < size; i ++) {
-			typename T::type::element_t el;
-			s.read(reinterpret_cast<char *>(&el), sizeof(el));
-			f.value.push_back(el);
-		}
-	}
+        for (size_type i = 0; i < size; i ++) {
+            typename T::type::element_t el;
+            s.read(reinterpret_cast<char *>(&el), sizeof(el));
+            f.value.push_back(el);
+        }
+    }
 
-	template<typename T>
-	typename std::enable_if<std::is_trivially_copyable<typename T::type>::value>::type
-	visit(T &f)
-	{
-		s.read(reinterpret_cast<char *>(&f.value), sizeof(f.value));
-	}
+    template<typename T>
+    typename std::enable_if<std::is_trivially_copyable<typename T::type>::value>::type
+    visit(T &f)
+    {
+        s.read(reinterpret_cast<char *>(&f.value), sizeof(f.value));
+    }
 };
 
 }
@@ -80,13 +80,13 @@ struct record_deserializer
 template<typename S, typename T>
 void serialize(S &s, T& record)
 {
-	record.visit(detail::record_serializer<S>{s});
+    record.visit(detail::record_serializer<S>{s});
 }
 
 template<typename S, typename T>
 void deserialize(S &s, T& record)
 {
-	record.visit(detail::record_deserializer<S>{s});
+    record.visit(detail::record_deserializer<S>{s});
 }
 
 }
