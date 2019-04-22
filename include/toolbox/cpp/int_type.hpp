508d4779af2892209ed3cd3fba899c59cb6c7d41
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-04-22 14:02:03 +0200

    retab

--- include/toolbox/cpp/int_type.hpp
+++ include/toolbox/cpp/int_type.hpp
@@ -11,23 +11,23 @@ namespace cpp
 
 template<std::size_t N> struct int_type
 {
-	template<bool C, typename T, typename F>
-	using cond = typename std::conditional<C,T,F>::type;
+    template<bool C, typename T, typename F>
+    using cond = typename std::conditional<C,T,F>::type;
 
-	using signed_type = 
-		cond<N <= 8, std::int8_t,
-			cond<N <= 16, std::int16_t,
-				cond<N <= 32, std::int32_t,
-					cond<N <= 64, std::int64_t, std::false_type>
-				>
-			>
-		>;
+    using signed_type =
+        cond<N <= 8, std::int8_t,
+            cond<N <= 16, std::int16_t,
+                cond<N <= 32, std::int32_t,
+                    cond<N <= 64, std::int64_t, std::false_type>
+                >
+            >
+        >;
 
-	static_assert(!std::is_same<signed_type, std::false_type>::value,
-		"wrong N, outside [1, 64] range");
+    static_assert(!std::is_same<signed_type, std::false_type>::value,
+        "wrong N, outside [1, 64] range");
 
-	// TODO: implement make_unsigned in type_traits
-	//using unsigned_type = typename std::make_unsigned<signed_type>::type;
+    // TODO: implement make_unsigned in type_traits
+    //using unsigned_type = typename std::make_unsigned<signed_type>::type;
 };
 
 } // namespace cpp
