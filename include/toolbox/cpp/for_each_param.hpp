a8a3cc8df0df501ab6429c04ddababa68749ec9f
Lukasz Gemborowski <lukasz.gemborowski@gmail.com>
2019-04-22 14:06:41 +0200

    retab

--- include/toolbox/cpp/for_each_param.hpp
+++ include/toolbox/cpp/for_each_param.hpp
@@ -12,14 +12,14 @@ namespace detail
 template<typename Func, typename Last>
 void for_each_param(Func &&func, Last &&last)
 {
-	func(last);
+    func(last);
 }
 
 template<typename Func, typename First, typename... Params>
 void for_each_param(Func &&func, First &&first, Params&&... params)
 {
-	func(first);
-	for_each_param(func, params...);
+    func(first);
+    for_each_param(func, params...);
 }
 
 } // namespace detail
@@ -27,8 +27,8 @@ void for_each_param(Func &&func, First &&first, Params&&... params)
 template<typename Func, typename... Params>
 auto for_each_param(Func &&func, Params&&... params)
 {
-	detail::for_each_param(func, params...);
-	return func;
+    detail::for_each_param(func, params...);
+    return func;
 }
 
 } // namespace cpp
