2baba957963ef7127a1f881e2425af387d8adff5
Łukasz Gemborowski <lukasz.gemborowski@gmail.com>
2020-02-21 00:18:55 +0100

    add text_file::save function

--- include/toolbox/text_file.hpp
+++ include/toolbox/text_file.hpp
@@ -27,6 +27,17 @@ std::string load(toolbox::fs::path path)
     return load(ifs);
 }
 
+void save(std::ofstream &file, const std::string &str)
+{
+    file.write(str.data(), str.size());
+}
+
+void save(fs::path path, const std::string &str)
+{
+    std::ofstream ofs{path};
+    save(ofs, str);
+}
+
 } // namespace toolbox::text_file
 
 #endif // TOOLBOX_TEXT_FILE_HPP
